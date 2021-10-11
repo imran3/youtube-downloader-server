bf7a5d2ed82d77e5814e1ed139d9262aea7993c0
Imran Azam <imranazam3@gmail.com>
2021-10-12 00:54:06 +0100

    delete CORS use

--- src/index.js
+++ src/index.js
@@ -7,16 +7,6 @@ const swaggerDocument = require("./swagger.json");
 const PORT = process.env.PORT || 5000;
 const app = express();
 app.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-app.use(function (_, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
-  );
-  next();
-});
 
 app.listen(PORT, () => {
   console.log("Server running at http://localhost:", PORT);
